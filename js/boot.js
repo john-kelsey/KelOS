51d0b682374c8fa5861a8455d805792f1a7925ec
Joe Kelly <johnkelsey.irungu@gmail.com>
2026-08-01 19:02:29 +0300

    First Steps

---
+++ js/boot.js
@@ -0,0 +1,8 @@
+const lines= [
+    "Loading kernel...",
+    "Starting services...",
+    "Launching desktop..."
+
+];
+document.getElementById("boot-screen").style.display = "none";
+document.getElementById("desktop").hidden =false;
